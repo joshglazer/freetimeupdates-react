54c81b0d999316028973ab16a9e6185491c8eb39
Josh Glazer <joshglazer@gmail.com>
2019-02-02 14:19:48 -0500

    clean up research page charts

--- pages/research.js
+++ pages/research.js
@@ -5,35 +5,57 @@ import { Chart } from 'react-google-charts';
 
 import Default from '../layouts/default'
 const meta = {  }
-const options = {
+const optionsBase = {
   colors: ['#faf06b', '#bfdef2'],
-  is3D: true,
   backgroundColor: 'transparent',
+  legend: {
+    position: 'none',
+  },
+}
+
+const optionsColumnChart = {
+  ...optionsBase,
   vAxis: {
+    title: 'Time of Day',
     gridlines: {
         color: 'transparent'
     },
     baselineColor: '#faf06b',
-    textStyle:{color: '#faf06b'},
+    textStyle: {
+      color: '#faf06b'
+    },
+    titleTextStyle: {
+      color: '#faf06b',
+      fontSize: 18,
+    }
   },
   hAxis: {
+    title: 'Hours Passed in the Day',
     gridlines: {
-        color: 'transparent'
+      color: 'transparent'
     },
     baselineColor: '#faf06b',
-    textStyle:{color: '#faf06b'},
+    textStyle: {
+      color: '#faf06b',
+    },
+    titleTextStyle: {
+      color: '#faf06b',
+      fontSize: 18,
+    }
+  },
+}
+
+const optionsPieChart = {
+  ...optionsBase,
+  chartArea: {
+    width: '100%',
+    height: '100%'
   },
   pieSliceTextStyle: {
     color: 'black',
   },
-  tooltip: {
-    isHtml: true
-  },
-  legend: {
-    textStyle: {
-      color: '#faf06b'
-    }
-  },
+  is3D: true,
+  pieSliceText: 'label',
 }
 
 class ResearchPage extends Component {
@@ -54,34 +76,33 @@ class ResearchPage extends Component {
               chartType="ColumnChart"
               height={400}
               width={"100%"}
-              legendToggle
-              options={options}
+              options={optionsColumnChart}
               data={[
-                ['Hours Passed in the day', 'Time of Day'],
-                ['0', 12],
-                ['1', 1],
-                ['2', 2],
-                ['3', 3],
-                ['4', 4],
-                ['5', 5],
-                ['6', 6],
-                ['7', 7],
-                ['8', 8],
-                ['9', 9],
-                ['10', 10],
-                ['11', 11],
-                ['12', 12],
-                ['13', 1],
-                ['14', 2],
-                ['15', 3],
-                ['16', 4],
-                ['17', 5],
-                ['18', 6],
-                ['19', 7],
-                ['20', 8],
-                ['21', 9],
-                ['22', 10],
-                ['23', 11],
+                ['Hours Passed in the Day', 'Time of Day', { role: "tooltip", type: "string", p: { html: true } }],
+                ['0', 12, 'When 0 hours have passed, it is 12AM'],
+                ['1', 1, 'When 1 hour has passed, it is 1AM'],
+                ['2', 2, 'When 2 hours have passed, it is 2AM'],
+                ['3', 3, 'When 3 hours have passed, it is 3AM'],
+                ['4', 4, 'When 4 hours have passed, it is 4AM'],
+                ['5', 5, 'When 5 hours have passed, it is 5AM'],
+                ['6', 6, 'When 6 hours have passed, it is 6AM'],
+                ['7', 7, 'When 7 hours have passed, it is 7AM'],
+                ['8', 8, 'When 8 hours have passed, it is 8AM'],
+                ['9', 9, 'When 9 hours have passed, it is 9AM'],
+                ['10', 10, 'When 10 hours have passed, it is 10AM'],
+                ['11', 11, 'When 11 hours have passed, it is 11AM'],
+                ['12', 12, 'When 12 hours have passed, it is 12PM'],
+                ['13', 1, 'When 13 hours have passed, it is 1PM'],
+                ['14', 2, 'When 14 hours have passed, it is 2PM'],
+                ['15', 3, 'When 15 hours have passed, it is 3PM'],
+                ['16', 4, 'When 16 hours have passed, it is 4PM'],
+                ['17', 5, 'When 17 hours have passed, it is 5PM'],
+                ['18', 6, 'When 18 hours have passed, it is 6PM'],
+                ['19', 7, 'When 19 hours have passed, it is 7PM'],
+                ['20', 8, 'When 20 hours have passed, it is 8PM'],
+                ['21', 9, 'When 21 hours have passed, it is 9PM'],
+                ['22', 10, 'When 22 hours have passed, it is 10PM'],
+                ['23', 11, 'When 23 hours have passed, it is 11PM'],
               ]}
             />
           </div>
@@ -91,11 +112,11 @@ class ResearchPage extends Component {
               chartType="PieChart"
               height={400}
               width={"100%"}
-              options={options}
+              options={optionsPieChart}
               data={[
-                ['Type', 'Percent'],
-                ['AM', 12],
-                ['PM', 12],
+                ['Type', 'Percent', { role: "tooltip", type: "string", p: { html: true } }],
+                ['AM', 12, '50% of all hours fall during the AM portion of the day'],
+                ['PM', 12,  '50% of all hours fall during the AM portion of the day'],
               ]}
             />
           </div>
