592000c916b3443a1ca8820bad37d63803d55d40
Josh Glazer <joshglazer@gmail.com>
2021-09-21 21:09:05 -0400

    feat(named-exports): removed `export default function` from components
    updated node-sass to version 5

--- components/header.js
+++ components/header.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
-export default () => (
+const header = () => (
   <header>
-    <Link href='/'><a className="logo"><img src='/static/logo.png' id='logo' alt='Free Time Updates'/></a></Link>
+    <Link href="/">
+      <a className="logo">
+        <img src="/static/logo.png" id="logo" alt="Free Time Updates" />
+      </a>
+    </Link>
     <h1>Free Time Updates</h1>
   </header>
-)
+);
+
+export default header;
--- components/meta.js
+++ components/meta.js
@@ -1,23 +1,51 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-const titleDefault = 'Free Time Updates';
-const descriptionDefault = 'Are you tired of paying top dollar for your time updates? Check here for free time updates, all the time, every time.'
+const titleDefault = "Free Time Updates";
+const descriptionDefault =
+  "Are you tired of paying top dollar for your time updates? Check here for free time updates, all the time, every time.";
 
-export default ({ props = { title, description } }) => (
+const meta = ({ props = { title, description } }) => (
   <div>
     <Head>
-      <title key='title'>{ titleDefault + ( props.title ? ` | ${props.title}` : '' ) }</title>
-      <meta key='description' name='description' content={props.description || descriptionDefault} />
+      <title key="title">
+        {titleDefault + (props.title ? ` | ${props.title}` : "")}
+      </title>
+      <meta
+        key="description"
+        name="description"
+        content={props.description || descriptionDefault}
+      />
 
-      <meta key="og:title" property="og:title" content={ titleDefault + ( props.title ? ` | ${props.title}` : '' ) } />
-      <meta key="og:description" property="og:description" content={props.description || descriptionDefault} />
-      <meta key="og:image" property="og:image" content='https://www.freetimeupdates.com/static/logo.png' />
+      <meta
+        key="og:title"
+        property="og:title"
+        content={titleDefault + (props.title ? ` | ${props.title}` : "")}
+      />
+      <meta
+        key="og:description"
+        property="og:description"
+        content={props.description || descriptionDefault}
+      />
+      <meta
+        key="og:image"
+        property="og:image"
+        content="https://www.freetimeupdates.com/static/logo.png"
+      />
 
-      <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Rubik"
+        rel="stylesheet"
+      />
 
-      <link rel="shortcut icon" href="/static/favicon.ico" type="image/vnd.microsoft.icon" />
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta charSet='utf-8' />
+      <link
+        rel="shortcut icon"
+        href="/static/favicon.ico"
+        type="image/vnd.microsoft.icon"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
     </Head>
   </div>
-)
+);
+
+export default meta;
